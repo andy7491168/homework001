a22adea2101f3390d84011f76f66486d2a7f853c
andy tseng <andytseng@17.media>
2025-03-09 22:17:06 +0800

    updated

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@ mkdir ./data
 docker-compose up -d
 sleep 3
 docker-compose exec php-fpm composer install
+chmod -R 777 ./
+mv ./code/env-example ./code/.env
 sleep 3
 docker-compose exec php-fpm php artisan migrate
 docker-compose exec php-fpm php artisan db:seed --class=LineItemSeeder
